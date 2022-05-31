cfad4e8481dbf1d35c5a8d7e08622aed65b64b38
moolnam <moolnam>
2022-05-31 17:02:03 +0900

    Initial Commit

---
+++ marvel-api-app/marvel_api_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  marvel_api_appApp.swift
+//  marvel-api-app
+//
+//  Created by KimJongHee on 2022/05/31.
+//
+
+import SwiftUI
+
+@main
+struct marvel_api_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
